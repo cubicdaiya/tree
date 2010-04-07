58c1d6e9c8f310d7cd00acf2f324c381c246d3fa
cubicdaiya <cubicdaiya@gmail.com>
2010-04-07 20:48:53 +0900

    add test create ignore file

--- test/bst_test.c
+++ test/bst_test.c
@@ -7,9 +7,6 @@
 static void bst_test001(void);
 static void bst_test002(void);
 static void bst_test003(void);
-static void bst_test004(void);
-static void bst_test005(void);
-
 
 static void bst_test001(void) {
   mpool_t *pool    = mpool_create(1024);
@@ -32,15 +29,24 @@ static void bst_test002(void) {
 }
 
 static void bst_test003(void) {
+  mpool_t *pool    = mpool_create(1024);
+  bst_node_t *root = bst_create(0, &pool);
+  CU_ASSERT(root->elem == 0);
+  bst_insert(root, 3,  &pool);
+  bst_insert(root, 8,  &pool);
+  bst_insert(root, 4,  &pool);
+  bst_insert(root, 5,  &pool);
+  bst_insert(root, 9,  &pool);
+  bst_insert(root, 7,  &pool);
+  bst_insert(root, 1,  &pool);
+  bst_insert(root, 2,  &pool);
+  bst_insert(root, 6,  &pool);
+  bst_insert(root, 10, &pool);
 
-}
-
-static void bst_test004(void) {
-
-}
-
-static void bst_test005(void) {
-
+  for (int i=0;i<11;++i) {
+    CU_ASSERT(bst_search(root, i));
+  }
+  bst_destroy(pool);
 }
 
 int main (int argc, char *argv[]) {
@@ -50,8 +56,6 @@ int main (int argc, char *argv[]) {
   CU_add_test(suite_bstree, "bstree_test001", bst_test001);
   CU_add_test(suite_bstree, "bstree_test002", bst_test002);
   CU_add_test(suite_bstree, "bstree_test003", bst_test003);
-  CU_add_test(suite_bstree, "bstree_test004", bst_test004);
-  CU_add_test(suite_bstree, "bstree_test005", bst_test005);
   CU_basic_run_tests();
   CU_cleanup_registry();
   
